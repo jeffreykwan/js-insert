b0ce0f339a81322136d4e373c4b2d5b3a3ef2d3f
Jeffrey Kwan <jeffureekwan@gmail.com>
2014-06-16 20:42:03 -0700

    Organized parser tests

--- test/parser_test.js
+++ test/parser_test.js
@@ -19,10 +19,18 @@ describe('Parser', function () {
       expect(node).eql(ast.body);
     });
 
-    it('should find nested function declaration with no identifier', function () {
-      var ast = esprima.parse('define(function () {});');
-      var node = parser.parse(ast, {func: 'define', nested: {func: true}});
-      expect(JSON.stringify(node)).to.contain('"type":"FunctionExpression","id":null');
+    describe('Nested Function Call', function () {
+      it('should find a nested function call in function declaration node', function () {
+        var ast = esprima.parse('function test() {test1(); test3();}');
+        var node = parser.parse(ast, {func: 'test', nested: {func: 'test1'}});
+        expect(node).eql(esprima.parse('test1();').body);
+      });
+
+      it('should find second function call in func declaration node', function () {
+        var ast = esprima.parse('function test() {test1(); test3();}');
+        var node = parser.parse(ast, {func: 'test', nested: {func: 'test3'}});
+        expect(node).eql(esprima.parse('test3();').body);
+      });
     });
   });
 
@@ -33,16 +41,12 @@ describe('Parser', function () {
       expect(node).eql(ast.body);
     });
 
-    it('should find a nested function call in function declaration node', function () {
-      var ast = esprima.parse('function test() {test1(); test3();}');
-      var node = parser.parse(ast, {func: 'test', nested: {func: 'test1'}});
-      expect(node).eql(esprima.parse('test1();').body);
-    });
-
-    it('should find second function call in func declaration node', function () {
-      var ast = esprima.parse('function test() {test1(); test3();}');
-      var node = parser.parse(ast, {func: 'test', nested: {func: 'test3'}});
-      expect(node).eql(esprima.parse('test3();').body);
+    describe('Nested Function Declaration', function () {
+      it('should find nested function declaration with no identifier', function () {
+        var ast = esprima.parse('define(function () {});');
+        var node = parser.parse(ast, {func: 'define', nested: {func: true}});
+        expect(JSON.stringify(node)).to.contain('"type":"FunctionExpression","id":null');
+      });
     });
   });
 
@@ -53,10 +57,20 @@ describe('Parser', function () {
       expect(node).eql(ast.body);
     });
 
-    it('should find nested method declaration node', function () {
-      var ast = esprima.parse('obj.f1 = function () {obj.f3 = function () {} }');
-      var node = parser.parse(ast, {obj: 'obj', func: 'f1', nested: {obj: 'obj', func: 'f3'}});
-      expect(node).to.eql(esprima.parse('obj.f3 = function () {}').body);
+    describe('Nested Method Declaration', function () {
+      it('should find nested method declaration node', function () {
+        var ast = esprima.parse('obj.f1 = function () {obj.f3 = function () {} }');
+        var node = parser.parse(ast, {obj: 'obj', func: 'f1', nested: {obj: 'obj', func: 'f3'}});
+        expect(node).to.eql(esprima.parse('obj.f3 = function () {}').body);
+      });
+    });
+
+    describe('Nested Method Call', function () {
+      it('should find nested method call inside method declaration', function () {
+        var ast = esprima.parse('obj.f1 = function () { obj.f2(); }');
+        var node = parser.parse(ast, {obj: 'obj', func: 'f1', nested: { obj: 'obj', func: 'f2'}});
+        expect(node).eql(esprima.parse('obj.f2()').body);
+      });
     });
 
   });
@@ -66,12 +80,6 @@ describe('Parser', function () {
       var ast = esprima.parse('obj.f1()');
       var node = parser.parse(ast, {obj: 'obj', func: 'f1'});
       expect(node).eql(ast.body);
-    });
-
-    it('should find nested method call inside method declaration', function () {
-      var ast = esprima.parse('obj.f1 = function () { obj.f2(); }');
-      var node = parser.parse(ast, {obj: 'obj', func: 'f1', nested: { obj: 'obj', func: 'f2'}});
-      expect(node).eql(esprima.parse('obj.f2()').body);
     });
   });
 
@@ -92,5 +100,4 @@ describe('Parser', function () {
       expect(node[0].key.name).to.equal('paths');
     });
   });
-
 });
