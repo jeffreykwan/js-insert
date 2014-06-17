aec85771b334fb7e302b96a962d46012c664ad2a
Jeffrey Kwan <jeffureekwan@gmail.com>
2014-06-16 21:21:27 -0700

    Organized Parameter tests

--- test/parameter_test.js
+++ test/parameter_test.js
@@ -7,9 +7,7 @@ var expect = require('expect.js');
 var parameter = require('../lib/parameter.js');
 
 describe('Parameter', function () {
-
-  describe('function declaration', function () {
-
+  describe('#Function Declaration', function () {
     it('should insert parameter into function declaration', function () {
       var ast = esprima.parse('function test() {}');
       parameter.add(ast.body[0], {param: 'a'});
@@ -23,8 +21,7 @@ describe('Parameter', function () {
     });
   });
 
-  describe('function call', function () {
-
+  describe('#Function Call', function () {
     it('should add parameter to a function call', function () {
       var ast = esprima.parse('f1();');
 
@@ -37,17 +34,6 @@ describe('Parameter', function () {
       expect(result).to.contain('f1(\'a\')');
     });
 
-    it('should add parameter to member call', function () {
-      var ast = esprima.parse('obj.f1(a)');
-
-      parameter.add(ast.body[0], {
-        param: 'b',
-        type: 'variable'
-      });
-
-      expect(gen.generate(ast)).to.contain('obj.f1(a, b)');
-    });
-
     it('should default add a variable parameter', function () {
       var ast = esprima.parse('f1();');
 
@@ -58,76 +44,95 @@ describe('Parameter', function () {
       expect(gen.generate(ast)).to.contain('f1(b)');
     });
 
-    it('should add parameter to array parameter', function () {
-      var ast = esprima.parse('f1([a])');
+    describe('Array Parameter', function () {
+      it('should add parameter to array parameter', function () {
+        var ast = esprima.parse('f1([a])');
 
-      parameter.add(ast.body[0], {
-        arr: {
-          param: 'b',
-          type: 'variable'
-        }
-      });
-
-      expect(gen.generate(ast).replace(/\s+/g, ' ')).to.contain('f1([ a, b ]);');
-    });
-
-    it('should add parameter to function parameter', function () {
-      var ast = esprima.parse('obj.f1(function (a) {})');
-
-      parameter.add(ast.body[0], {
-        func: {
-          param: 'b',
-          type: 'variable'
-        }
-      });
-
-      expect(gen.generate(ast)).to.contain('obj.f1(function (a, b)');
-    });
-
-    it('should insert into an object parameter', function () {
-      var ast = esprima.parse('obj.f1({a: \'hi\'});');
-
-      parameter.add(ast.body[0], {
-        obj: {
-          key: 'b',
-          value: 'bye'
-        }
-      });
-
-      expect(gen.generate(ast).replace(/\s+/g, ' ')).to.contain('a: \'hi\', b: bye');
-    });
-
-    it('should insert into an object parameter with a string', function () {
-      var ast = esprima.parse('obj.f1({a: \'hi\'});');
-
-      parameter.add(ast.body[0], {
-        obj: {
-          key: 'b',
-          value: 'bye',
-          type: 'literal'
-        }
-      });
-
-      expect(gen.generate(ast).replace(/\s+/g, ' ')).to.contain('a: \'hi\', b: \'bye\'');
-    });
-
-    xit('should insert into an object two levels in', function () {
-      var ast = esprima.parse('obj.f1({parse: {}})');
-
-      parameter.funcCall(ast, {
-        obj: 'obj',
-        func: 'f1'
-      }, {
-        obj: {
-          obj: {
-            name: 'parse',
-            key: 'a',
-            value: 'bbc'
+        parameter.add(ast.body[0], {
+          arr: {
+            param: 'b',
+            type: 'variable'
           }
-        }
+        });
+
+        expect(gen.generate(ast).replace(/\s+/g, ' ')).to.contain('f1([ a, b ]);');
+      });
+    });
+
+    describe('Function Parameter', function () {
+      it('should add parameter to function parameter', function () {
+        var ast = esprima.parse('obj.f1(function (a) {})');
+
+        parameter.add(ast.body[0], {
+          func: {
+            param: 'b',
+            type: 'variable'
+          }
+        });
+
+        expect(gen.generate(ast)).to.contain('obj.f1(function (a, b)');
+      });
+    });
+
+    describe('Object Parameter', function () {
+      it('should insert into an object parameter', function () {
+        var ast = esprima.parse('obj.f1({a: \'hi\'});');
+
+        parameter.add(ast.body[0], {
+          obj: {
+            key: 'b',
+            value: 'bye'
+          }
+        });
+
+        expect(gen.generate(ast).replace(/\s+/g, ' ')).to.contain('a: \'hi\', b: bye');
       });
 
-      expect(gen.generate(ast).replace(/\s+/g, ' ')).to.contain('parse: { a: \'bbc\'');
+      it('should insert into an object parameter with a string', function () {
+        var ast = esprima.parse('obj.f1({a: \'hi\'});');
+
+        parameter.add(ast.body[0], {
+          obj: {
+            key: 'b',
+            value: 'bye',
+            type: 'literal'
+          }
+        });
+
+        expect(gen.generate(ast).replace(/\s+/g, ' ')).to.contain('a: \'hi\', b: \'bye\'');
+      });
+
+      xit('should insert into an object two levels in', function () {
+        var ast = esprima.parse('obj.f1({parse: {}})');
+
+        parameter.funcCall(ast, {
+          obj: 'obj',
+          func: 'f1'
+        }, {
+          obj: {
+            obj: {
+              name: 'parse',
+              key: 'a',
+              value: 'bbc'
+            }
+          }
+        });
+
+        expect(gen.generate(ast).replace(/\s+/g, ' ')).to.contain('parse: { a: \'bbc\'');
+      });
+    });
+  });
+
+  describe('#Method Call', function () {
+    it('should add parameter to member call', function () {
+      var ast = esprima.parse('obj.f1(a)');
+
+      parameter.add(ast.body[0], {
+        param: 'b',
+        type: 'variable'
+      });
+
+      expect(gen.generate(ast)).to.contain('obj.f1(a, b)');
     });
   });
 });
